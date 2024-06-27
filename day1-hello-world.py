3196ea5263c6f21c14e0d0ed2878a97a69d073f1
novferdianto <novferdianto@gmail.com>
2024-06-27 04:32:56 +0000

    add more variance using print() in python

--- day1-hello-world.py
+++ day1-hello-world.py
@@ -1,3 +1,19 @@
 #This is how using print() function in python
 #Always rename to main.py for replit to run
 print("Hello World")
+
+#This is how using print() function and concatenating strings in python
+print("Hello " + "World")
+
+#This is how using print() function, concating strings and adding double quotes in python
+print("Hello " + "World ." + "this is using \"double quotes\"")
+
+#This is how using print() function and taking input from user in python
+print("Hello " + input("What is your name ? "))
+
+#This is how using print() function and taking input from user and calculating the length of the input in python
+print("Hello " +  " your name has " + str(len(input("What is your name ? "))) + " characters") 
+
+#This is how using print() function and taking input from user and store into variable in python
+name = input("What is your name ? ")
+print("Hello " + name)
