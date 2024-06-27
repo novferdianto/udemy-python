6783812c014056dd37f296bbef89707e4bc7fe8e
novferdianto <novferdianto@gmail.com>
2024-06-27 04:54:26 +0000

    put variables , using temporary variables in python
    put variables , using temporary variables in python

--- day1-hello-world.py
+++ day1-hello-world.py
@@ -17,3 +17,17 @@ print("Hello " +  " your name has " + str(len(input("What is your name ? "))) + 
 #This is how using print() function and taking input from user and store into variable in python
 name = input("What is your name ? ")
 print("Hello " + name)
+
+#This is how using print() function and taking input from user, store into variable and calculating the length of the input from the variable in python
+nama = input("What is your name ? ")
+length = len(nama)
+print(length)
+
+#This is how using temporary variable in python
+var_a = input("variable a: ")
+var_b = input("variable b: ")
+temp = var_a
+var_a = var_b
+var_b = temp
+print("variable a : " + var_a)
+print("variable b : " + var_b)
