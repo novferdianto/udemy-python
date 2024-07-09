ad9dda0789b6bcf13e45bac5ae3654caf56e79f4
novferdianto <novferdianto@gmail.com>
2024-07-09 12:31:12 +0000

    lap-year, odd-even-challenge and rollercoaster 1.2

--- main.py
+++ main.py
@@ -8,16 +8,26 @@
 
 print("Welcome to rollercoaster height check program \n")
 height = int(input("What's your height in cm ? "))
+bill = 0
+
 if height >= 120:
   print("You can ride the rollercoaster")
   age = int(input("What's your age ? "))
   if age < 12:
-    print("Please pay IDR 50,000")
+    # print("Please pay IDR 50,000")
+    bill = 50000
   elif age < 15:
-    print("Please pay IDR 60,000")
+    # print("Please pay IDR 60,000")
+    bill = 60000
   elif age <= 18:
-    print("Please pay IDR 70,000")
+    # print("Please pay IDR 70,000")
+    bill = 70000
   else:
-    print("Please pay IDR 100,000")
+    # print("Please pay IDR 100,000")
+    bill = 100000
+  photo = input ("Do you want a photo taken ? Y or N. ")
+  if photo == "Y":
+    bill +=25000
+  print (f"Your final bill is IDR {bill}")
 else:
   print("You can't ride the rollercoaster")
